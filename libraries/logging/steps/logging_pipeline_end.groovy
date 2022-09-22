9af103f8e6de310f4a5b221b56d3650781f11f52
Kirill Borisov <kirillborisov@Kirills-MacBook-Pro.local>
2022-09-22 12:37:13 +0300

    rename file

--- libraries/logging/steps/logging_pipeline_end.groovy
+++ libraries/logging/steps/logging_pipeline_end.groovy
@@ -1,8 +1,4 @@
-import logging.Utility
-
 @CleanUp
 void call(hookContext){
-    Utility u = new Utility()
-    u.doThing(steps)
     println "End log event of the pipeline!" 
 }
--- libraries/maven/steps/build.groovy
+++ libraries/maven/steps/build.groovy
@@ -1,6 +1,12 @@
+import logging.Utility
+
 void call(){
     def mavenCredentials = config.mavenCredentials ?: "maven"
     stage("Maven: Build"){
+        script {
+            Utility u = new Utility()
+            u.doThing(steps)
+        }
         withMaven(maven: '3.8.6') {
                 sh 'mvn clean install -DskipTests=true'
                 println mavenCredentials
