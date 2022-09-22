ff69b8c753c16f98dfe8b5cae7fcfec6192d39b7
Kirill Borisov <kirillborisov@Kirills-MacBook-Pro.local>
2022-09-22 11:51:44 +0300

    rename file

--- libraries/splunk/steps/splunk_step_watcher.groovy
+++ libraries/splunk/steps/splunk_step_watcher.groovy
@@ -1,9 +1,9 @@
 @BeforeStep({ hookContext.library == "maven" && hookContext.step == "build" })
 void call(){
-  println "running after the ${hookContext.library}'s ${hookContext.step} step"
+  println "running before step"
 }
 
 @AfterStep({ hookContext.library == "maven" && hookContext.step == "build" })
 void call(){
-  println "running after the ${hookContext.library}'s ${hookContext.step} step"
+  println "running after step"
 }
