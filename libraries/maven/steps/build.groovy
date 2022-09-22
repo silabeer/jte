66621c95b49f74930d96bd9b6587cf1a669306c9
Kirill Borisov <kirillborisov@Kirills-MacBook-Pro.local>
2022-09-22 10:12:00 +0300

    rename file

--- libraries/maven/steps/build.groovy
+++ libraries/maven/steps/build.groovy
@@ -1,9 +1,7 @@
 void call(){
     stage("Maven: Build"){
-        withMaven(maven: '3.8.6',  mavenSettingsConfig: 'my-project', mavenOpts: '${ADDITIONAL_MVN_OPTS}', options: [artifactsPublisher(disabled: true)]) {
-            withSonarQubeEnv(installationName: 'sq') {
+        withMaven(maven: '3.8.6') {
                 sh 'mvn clean install -DskipTests=true'
-            }
         } 
     }
 }
