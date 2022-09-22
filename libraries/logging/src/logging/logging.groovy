068ab7f1801540104e6184c87c64c7186e7a47c0
Kirill Borisov <kirillborisov@Kirills-MacBook-Pro.local>
2022-09-22 12:35:24 +0300

    rename file

---
+++ libraries/logging/src/logging/logging.groovy
@@ -0,0 +1,7 @@
+package logging
+
+class Utility implements Serializable{
+  void doThing(steps){
+    steps.echo "message from the Utility class"
+  }
+}
--- libraries/logging/steps/logging_pipeline_end.groovy
+++ libraries/logging/steps/logging_pipeline_end.groovy
@@ -1,4 +1,8 @@
+import logging.Utility
+
 @CleanUp
 void call(hookContext){
+    Utility u = new Utility()
+    u.doThing(steps)
     println "End log event of the pipeline!" 
 }
