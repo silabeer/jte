f70307a2c5fbe78346ff33a26e9af4a8b5f7a032
Kirill Borisov <kirillborisov@Kirills-MacBook-Pro.local>
2022-09-22 11:39:41 +0300

    rename file

--- libraries/splunk/steps/splunk_step_watcher.groovy
+++ libraries/splunk/steps/splunk_step_watcher.groovy
@@ -1,14 +1,14 @@
 @BeforeStep
 void before(hookContext){
-  println "Splunk: running " 
+  println "Splunk: running on $hookContext.step" 
 }
 
 @AfterStep({ config.afterSteps ? (hookContext.step in config.afterSteps) : true })
 void after(hookContext){
-  println "Splunk: running" 
+  println "Splunk: running on $hookContext.step" 
 }
 
 @AfterStep({ currentBuild.result.toString() == "FAILURE" })
 void afterFailure(hookContext){
-  println "Splunk: running"  
+  println "Splunk: running on $hookContext.step"  
 }
